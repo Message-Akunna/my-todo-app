13b8f82711ec478253bb95b0655225b550c6f9b1
Message Akunna <message.ao@gmail.com>
2020-07-12 17:07:50 +0100

    script updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,62 @@
 (function () {
-    'use strict'
-    const taskListItemSelector = '.task-list-item';
-    var taskListItem = document.querySelectorAll('.task-list-item');
+    //'use strict'
+    const taskListItemSelector = '[data-draggable="item"]';
+    const taskListsSelector = '[data-draggable="target"]';
+    //var taskListItem = document.querySelectorAll('.task-list-item');
     
-    //
-    taskListItem.forEach(item => item.addEventListener('dragstart', function(event) {
-        if(!event.target.matches(taskListItemSelector) || !event.target.closest(taskListItemSelector).matches(taskListItemSelector) ){
-            return;
+    //exclude older browsers by the features we need them to support
+    //and legacy opera explicitly so we don't waste time on a dead browser
+    if(
+        !document.querySelectorAll || !('draggable' in document.createElement('span')) || window.opera
+    ) { return; }
+    
+    //get the collection of draggable items and add their draggable attribute
+    for(var items = document.querySelectorAll(taskListItemSelector), len = items.length, i = 0; i < len; i ++){
+        items[i].setAttribute('draggable', 'true');
+    }
+
+    //variable for storing the dragging item reference 
+    //this will avoid the need to define any transfer data 
+    //which means that the elements don't need to have IDs 
+    var item = null;
+
+    //dragstart event to initiate mouse dragging
+    document.addEventListener('dragstart', function(e){
+        //set the item reference to this element
+        item = e.target;
+        
+        //we don't need the transfer data, but we have to define something
+        //otherwise the drop action won't work at all in firefox
+        //most browsers support the proper mime-type syntax, eg. "text/plain"
+        //but we have to use this incorrect syntax for the benefit of IE10+
+        e.dataTransfer.setData('text', '');
+    
+    }, false);
+
+    //dragover event to allow the drag by preventing its default
+    //ie. the default action of an element is not to allow dragging 
+    document.addEventListener('dragover', function(e){
+        if(item){
+            e.preventDefault();
         }
-        event.dataTransfer.setData('text/plain', event.target.id);
-        event.currentTarget.style.backgroundColor = '#efefef';
-    }));
+    }, false);  
 
-    //
-    document.getElementById('progressTaskList').addEventListener('dragover', function(event){
-        event.preventDefault();
-    });
+    //drop event to allow the element to be dropped into valid targets
+    document.addEventListener('drop', function(e){
+        //if this element is a drop target, move the item here 
+        //then prevent default to allow the action (same as dragover)
+        // console.log(e.target);
+        
+        if(e.target.getAttribute('data-draggable') == 'target' || e.target.closest(taskListsSelector).matches(taskListsSelector)){
+            e.target.closest('[data-draggable=target]').appendChild(item);  
+            e.preventDefault();
+        }
+    }, false);
     
-    //
-    document.getElementById('progressTaskList').addEventListener('drop', function(event){
-        // if(!event.target.matches(taskListItemSelector)) {
-        //     return;
-        // }
-        const id = event.dataTransfer.getData('text');
-
-        const draggableElement = document.getElementById(id);
-        const dropzone = event.target.closest('#progressTaskList');
-
-        dropzone.appendChild(draggableElement);
-
-        event.dataTransfer.clearData();
-    });
+    //dragend event to clean-up after drop or abort
+    //which fires whether or not the drop target was valid
+    document.addEventListener('dragend', function(e){
+        item = null;
+    }, false);
 
 }())
